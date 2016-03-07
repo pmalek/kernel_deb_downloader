9aa3c6621590489d9d72a876542db9201d7dc6aa
Patryk Małek <malekpatryk@gmail.com>
2016-03-07 20:58:30 +0100

    Adding versionutils tests

--- versionutils/versionutils_test.go
+++ versionutils/versionutils_test.go
@@ -19,3 +19,35 @@ func Test_UnifiedVersion2(t *testing.T) {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
 	}
 }
+
+func Test_IsAnRcVersion1(t *testing.T) {
+	actual := IsAnRCVersion("v4.5-rc3-wily/")
+	const expected = true
+	if actual != expected {
+		t.Errorf("Expected: '%s', got '%s'", expected, actual)
+	}
+}
+
+func Test_IsAnRcVersion2(t *testing.T) {
+	actual := IsAnRCVersion("v4.4.3-wily/")
+	const expected = false
+	if actual != expected {
+		t.Errorf("Expected: '%s', got '%s'", expected, actual)
+	}
+}
+
+func Test_IsAnRcVersion3(t *testing.T) {
+	actual := IsAnRCVersion("v4.0-rc7-vivid/")
+	const expected = true
+	if actual != expected {
+		t.Errorf("Expected: '%s', got '%s'", expected, actual)
+	}
+}
+
+func Test_IsAnRcVersion4(t *testing.T) {
+	actual := IsAnRCVersion("v4.1.9-unstable/")
+	const expected = false
+	if actual != expected {
+		t.Errorf("Expected: '%s', got '%s'", expected, actual)
+	}
+}

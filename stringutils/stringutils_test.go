2a5dce499a42ebb98b90a3b0fb6a5eaa2b34ac35
Patryk Małek <malekpatryk@gmail.com>
2016-06-06 20:27:01 +0200

    Changing stringutils tests to table driven tests

--- stringutils/stringutils_test.go
+++ stringutils/stringutils_test.go
@@ -4,106 +4,106 @@ import (
 	"testing"
 )
 
-func Test_Stripchars_strips_properly(t *testing.T) {
-	actual := Stripchars("qwerty.asdfgh.", ".")
-	const expected = "qwertyasdfgh"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
+type stripCharsTestData struct {
+	str      string
+	chr      string
+	expected string
+}
+
+var stripCharsTests = []stripCharsTestData{
+	{"qwerty.asdfgh.", ".", "qwertyasdfgh"},
+	{"qwerty.asdfgh.", ",", "qwerty.asdfgh."},
+	{"qwerty.asdfgh.", " ", "qwerty.asdfgh."},
+	{"abc", ".qwe", "abc"},
+	{"123456", "123456", ""},
+	{"1234567", "123456", "7"},
+	{"1234567", "123456.", "7"},
+}
+
+func Test_Stripchars(t *testing.T) {
+	for _, tt := range stripCharsTests {
+		actual := Stripchars(tt.str, tt.chr)
+		if actual != tt.expected {
+			t.Errorf("Expected: '%s', got '%s'", tt.expected, actual)
+		}
 	}
 }
 
-func Test_Stripchars_doesnt_strip_unspecified_characters(t *testing.T) {
-	actual := Stripchars("abc", ".qwe")
-	const expected = "abc"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
+type padTestData struct {
+	s        string
+	padStr   string
+	pLen     int
+	expected string
+}
+
+var leftPadTests = []padTestData{
+	{"aa", "0", 3, "000aa"},
+	{"abc", "g", 3, "gggabc"},
+	{"1", "0", 0, "1"},
+	{"TEST", "0", 5, "00000TEST"},
+	{"TEST", "xx", 3, "xxxxxxTEST"},
+	{"TEST", "xx", 25, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxTEST"},
+}
+
+func Test_LeftPad(t *testing.T) {
+	for _, tt := range leftPadTests {
+		actual := LeftPad(tt.s, tt.padStr, tt.pLen)
+		if actual != tt.expected {
+			t.Errorf("Expected: '%s', got '%s'", tt.expected, actual)
+		}
 	}
 }
 
-func Test_Stripchars_stripping_all_characters_gives_empty_string(t *testing.T) {
-	actual := Stripchars("123456", "123456")
-	const expected = ""
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
+var rightPadTests = []padTestData{
+	{"aa", "0", 3, "aa000"},
+	{"abc", "g", 3, "abcggg"},
+	{"1", "0", 0, "1"},
+	{"TEST", "0", 5, "TEST00000"},
+	{"TEST", "xx", 3, "TESTxxxxxx"},
+	{"TEST", "xx", 20, "TESTxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"},
+}
+
+func Test_RightPad(t *testing.T) {
+	for _, tt := range rightPadTests {
+		actual := RightPad(tt.s, tt.padStr, tt.pLen)
+		if actual != tt.expected {
+			t.Errorf("Expected: '%s', got '%s'", tt.expected, actual)
+		}
 	}
 }
 
-func Test_LeftPad1(t *testing.T) {
-	actual := LeftPad("aa", "0", 3)
-	const expected = "000aa"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
+var leftPad2LenTests = []padTestData{
+	{"aa", "0", 3, "0aa"},
+	{"abc", "g", 5, "ggabc"},
+	{"1", "0", 1, "1"},
+	{"1", "0", 0, ""},
+	{"TEST", "0", 5, "0TEST"},
+	{"TEST", "0", 50, "0000000000000000000000000000000000000000000000TEST"},
+}
+
+func Test_LeftPad2Len(t *testing.T) {
+	for _, tt := range leftPad2LenTests {
+		actual := LeftPad2Len(tt.s, tt.padStr, tt.pLen)
+		if actual != tt.expected {
+			t.Errorf("Expected: '%s', got '%s'", tt.expected, actual)
+		}
 	}
 }
 
-func Test_LeftPad2(t *testing.T) {
-	actual := LeftPad("abc", "g", 3)
-	const expected = "gggabc"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
-	}
+var rightPad2LenTests = []padTestData{
+	{"aa", "0", 3, "aa0"},
+	{"abc", "g", 5, "abcgg"},
+	{"1", "0", 1, "1"},
+	{"1", "0", 0, ""},
+	{"TEST", "0", 5, "TEST0"},
+	{"TEST", "0", 50, "TEST0000000000000000000000000000000000000000000000"},
 }
 
-func Test_LeftPad3(t *testing.T) {
-	actual := LeftPad("1", "0", 0)
-	const expected = "1"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
-	}
-}
-
-func Test_RightPad1(t *testing.T) {
-	actual := RightPad("abc", "g", 3)
-	const expected = "abcggg"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
-	}
-}
-
-func Test_RightPad2(t *testing.T) {
-	actual := RightPad("1", "0", 5)
-	const expected = "100000"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
-	}
-}
-
-func Test_RightPad3(t *testing.T) {
-	actual := RightPad("1", "0", 0)
-	const expected = "1"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
-	}
-}
-
-func Test_LeftPad2Len1(t *testing.T) {
-	actual := LeftPad2Len("1", "0", 10)
-	const expected = "0000000001"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
-	}
-}
-
-func Test_LeftPad2Len2(t *testing.T) {
-	actual := LeftPad2Len("1", "a", 5)
-	const expected = "aaaa1"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
-	}
-}
-
-func Test_RightPad2Len1(t *testing.T) {
-	actual := RightPad2Len("1", "0", 10)
-	const expected = "1000000000"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
-	}
-}
-
-func Test_RightPad2Len2(t *testing.T) {
-	actual := RightPad2Len("abc", "1", 5)
-	const expected = "abc11"
-	if actual != expected {
-		t.Errorf("Expected: '%s', got '%s'", expected, actual)
+func Test_RightPad2Len(t *testing.T) {
+	for _, tt := range rightPad2LenTests {
+		actual := RightPad2Len(tt.s, tt.padStr, tt.pLen)
+		if actual != tt.expected {
+			t.Errorf("Expected: '%s', got '%s'", tt.expected, actual)
+		}
 	}
 }

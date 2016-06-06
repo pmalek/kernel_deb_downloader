b8c9ec26a2ff100b2ac59926429bd185513e16d2
Patryk Małek <malekpatryk@gmail.com>
2016-06-06 19:59:07 +0200

    Minor changes in stringutils tests

--- stringutils/stringutils_test.go
+++ stringutils/stringutils_test.go
@@ -5,8 +5,7 @@ import (
 )
 
 func Test_Stripchars_strips_properly(t *testing.T) {
-	const original = "qwerty.asdfgh."
-	actual := Stripchars(original, ".")
+	actual := Stripchars("qwerty.asdfgh.", ".")
 	const expected = "qwertyasdfgh"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -14,17 +13,15 @@ func Test_Stripchars_strips_properly(t *testing.T) {
 }
 
 func Test_Stripchars_doesnt_strip_unspecified_characters(t *testing.T) {
-	const original = "abc"
-	actual := Stripchars(original, ".qwe")
-	const expected = original
+	actual := Stripchars("abc", ".qwe")
+	const expected = "abc"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
 	}
 }
 
 func Test_Stripchars_stripping_all_characters_gives_empty_string(t *testing.T) {
-	const original = "123456"
-	actual := Stripchars(original, "123456")
+	actual := Stripchars("123456", "123456")
 	const expected = ""
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -32,8 +29,7 @@ func Test_Stripchars_stripping_all_characters_gives_empty_string(t *testing.T) {
 }
 
 func Test_LeftPad1(t *testing.T) {
-	const original = "aa"
-	actual := LeftPad(original, "0", 3)
+	actual := LeftPad("aa", "0", 3)
 	const expected = "000aa"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -41,8 +37,7 @@ func Test_LeftPad1(t *testing.T) {
 }
 
 func Test_LeftPad2(t *testing.T) {
-	const original = "abc"
-	actual := LeftPad(original, "g", 3)
+	actual := LeftPad("abc", "g", 3)
 	const expected = "gggabc"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -50,8 +45,7 @@ func Test_LeftPad2(t *testing.T) {
 }
 
 func Test_LeftPad3(t *testing.T) {
-	const original = "1"
-	actual := LeftPad(original, "0", 0)
+	actual := LeftPad("1", "0", 0)
 	const expected = "1"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -59,8 +53,7 @@ func Test_LeftPad3(t *testing.T) {
 }
 
 func Test_RightPad1(t *testing.T) {
-	const original = "abc"
-	actual := RightPad(original, "g", 3)
+	actual := RightPad("abc", "g", 3)
 	const expected = "abcggg"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -68,8 +61,7 @@ func Test_RightPad1(t *testing.T) {
 }
 
 func Test_RightPad2(t *testing.T) {
-	const original = "1"
-	actual := RightPad(original, "0", 5)
+	actual := RightPad("1", "0", 5)
 	const expected = "100000"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -77,8 +69,7 @@ func Test_RightPad2(t *testing.T) {
 }
 
 func Test_RightPad3(t *testing.T) {
-	const original = "1"
-	actual := RightPad(original, "0", 0)
+	actual := RightPad("1", "0", 0)
 	const expected = "1"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -86,8 +77,7 @@ func Test_RightPad3(t *testing.T) {
 }
 
 func Test_LeftPad2Len1(t *testing.T) {
-	const original = "1"
-	actual := LeftPad2Len(original, "0", 10)
+	actual := LeftPad2Len("1", "0", 10)
 	const expected = "0000000001"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -95,8 +85,7 @@ func Test_LeftPad2Len1(t *testing.T) {
 }
 
 func Test_LeftPad2Len2(t *testing.T) {
-	const original = "1"
-	actual := LeftPad2Len(original, "a", 5)
+	actual := LeftPad2Len("1", "a", 5)
 	const expected = "aaaa1"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -104,8 +93,7 @@ func Test_LeftPad2Len2(t *testing.T) {
 }
 
 func Test_RightPad2Len1(t *testing.T) {
-	const original = "1"
-	actual := RightPad2Len(original, "0", 10)
+	actual := RightPad2Len("1", "0", 10)
 	const expected = "1000000000"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)
@@ -113,8 +101,7 @@ func Test_RightPad2Len1(t *testing.T) {
 }
 
 func Test_RightPad2Len2(t *testing.T) {
-	const original = "abc"
-	actual := RightPad2Len(original, "1", 5)
+	actual := RightPad2Len("abc", "1", 5)
 	const expected = "abc11"
 	if actual != expected {
 		t.Errorf("Expected: '%s', got '%s'", expected, actual)

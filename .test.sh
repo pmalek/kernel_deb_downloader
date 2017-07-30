0a150dc2c1456f8ad2405e56abf7c602f5ebaa4f
Patryk Małek <malekpatryk@gmail.com>
2017-07-30 21:28:26 +0200

    Update .test.sh script

--- .test.sh
+++ .test.sh
@@ -4,9 +4,8 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -v -race -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
+  go test -v -race -coverprofile=profile.out -covermode=atomic $d
+  if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+  fi
 done
